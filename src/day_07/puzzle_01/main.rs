afe6645811b5747adcbef001e16372414682cc49
borja_arnaez <borjaarpe@protonmail.com>
2024-12-12 14:02:52 +0100

    Finished day 7
    Separated solutions by files
    14 out of 50 stars

--- src/day_07/puzzle_01/main.rs
+++ src/day_07/puzzle_01/main.rs
@@ -16,28 +16,22 @@ fn valid_ops(op: &Vec<i64>) -> bool {
     _valid_ops(op[0], &op[2..], op[1])
 }
 
-fn concat_nums(n: i64, m: i64) -> i64 {
-    format!("{n}{m}").parse().unwrap()
-}
-
 fn _valid_ops(result: i64, vec: &[i64], acc: i64) -> bool {
     match acc.cmp(&result) {
-        Ordering::Greater => return false,
+        Ordering::Greater => false,
         Ordering::Equal => {
             if vec.len() == 0 {
                 return true;
             }
+            _valid_ops(result, &vec[1..], acc * vec[0]) || _valid_ops(result, &vec[1..], acc + vec[0])
         }
         Ordering::Less => {
             if vec.len() == 0 {
                 return false;
             }
+            _valid_ops(result, &vec[1..], acc * vec[0]) || _valid_ops(result, &vec[1..], acc + vec[0])
         }
-    };
-
-    _valid_ops(result, &vec[1..], acc * vec[0])
-        || _valid_ops(result, &vec[1..], acc + vec[0])
-        || _valid_ops(result, &vec[1..], concat_nums(acc, vec[0]))
+    }
 }
 
 fn parse_file() -> Vec<Vec<i64>> {
